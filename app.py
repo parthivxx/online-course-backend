5042af0b9ce09dcfa229ce4017646725a8d2d0dd
parthivxx <parthiv.nahata@gmail.com>
2025-02-12 20:05:03 +0530

    Going on Production

--- app.py
+++ app.py
@@ -8,13 +8,18 @@ from flask_cors import CORS
 
 app = Flask(__name__) 
 CORS(app , resources={r"/api/*": {"origins": "*"}})
+@app.route("/" , methods=['GET'])
+def hello():
+     return "Welcome to Online course backend server!!"
+
 app.register_blueprint(auth_bp , url_prefix="/api/auth")
 app.register_blueprint(upload_bp , url_prefix="/api")
 app.register_blueprint(courses_bp , url_prefix="/api")
 app.teardown_appcontext(teardown_db)
 
+
 with app.app_context():
      Base.metadata.create_all(bind=engine)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
